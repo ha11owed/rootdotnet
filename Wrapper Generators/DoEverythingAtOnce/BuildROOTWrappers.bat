264ffade8e89438f34dd0df500c9cce4b40eec4d
gwatts <gwatts@localhost>
2010-12-27 02:44:12 +0000

    Alter the production script s.t. it builds an include/lib directory, and populates them correctly.

--- Wrapper Generators/DoEverythingAtOnce/BuildROOTWrappers.bat
+++ Wrapper Generators/DoEverythingAtOnce/BuildROOTWrappers.bat
@@ -3,13 +3,16 @@ REM Build the root wrappers
 REM argument 1: where ROOTSYS should point
 REM argument 2: The output directory name
 REM argument 3: Where to copy the results. If blank, no copy is done.
+REM
+REM Expect to be run from the Wrapper Generators directory
+REM
 
 REM Setup the environment
 set "ROOTSYS=%1"
 set "PATH=%1\bin;%PATH%"
 
 REM Build everything so we can actually run programs! :-)
-echo "Building code to do the translation..."
+echo Building code to do the translation...
 devenv /nologo "Wrapper Generators.sln" /project "FindBadRootHeaders" /build "Release|Win32"
 devenv /nologo "Wrapper Generators.sln" /project "ROOT.NET Library Converter" /build "Release|Win32"
 
@@ -41,4 +44,8 @@ REM Now, see if we are going to copy the thing to a nice resting place.
 :justcopy
 if "%3"=="" goto :EOF
 if not exist "%3" mkdir "%3"
-copy ..\Wrappers\%2\Release\* "%3"
+if not exist "%3\lib" mkdir "%3\lib"
+if not exist "%3\include" mkdir "%3\include"
+copy ..\Wrappers\%2\Release\* "%3\lib"
+copy ..\Wrappers\WrapperPlumbingLibrary\NetArrayTranslator.hpp "%3\include"
+copy ..\Wrappers\%2\converted_items.txt "%3"
