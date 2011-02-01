3949e42f8e1373e157b4a255652f2eda52b89009
gwatts <gwatts@localhost>
2011-02-01 22:54:02 +0000

    Update to study some small things.

--- Wrapper Generators/ROOT.NET Monolithic Converter/Monolithic.cpp
+++ Wrapper Generators/ROOT.NET Monolithic Converter/Monolithic.cpp
@@ -72,11 +72,13 @@ int main()
 
 #ifndef notyet
 	/// The below lines are used during debugging in order to build a single (problem) class.
+	asked_for_class_list.push_back ("TObject");
+	asked_for_class_list.push_back ("TFile");
 	asked_for_class_list.push_back ("TSystem");
-	asked_for_class_list.push_back ("TWinNTSystem");
 
 	/// Make sure the libraries that are going to be needed are loaded!
 	libraries_to_load.push_back ("libCore");
+	libraries_to_load.push_back ("libRIO");
 
 #ifdef later
 	libraries_to_load.push_back ("libHist");
