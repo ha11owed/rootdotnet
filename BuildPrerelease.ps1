eff3d0cce70e6dee1331376ae602b594df9f6ff6
gwatts <gwatts@localhost>
2012-04-20 22:56:06 -0700

    Get the proper ROOT version info for what was given in the command line.

--- BuildPrerelease.ps1
+++ BuildPrerelease.ps1
@@ -1,4 +1,30 @@
 ﻿#
 # Coordinate building a pre-release version
+# The version shoudl be "5.33.02" or "5.33.02-rc1" or similar.
 #
-Write-Host "hi"
+[CmdletBinding()]
+param (
+  [Parameter(mandatory=$true)]
+  [string] $rootVersion
+)
+
+#
+# First, find the full info for the ROOT version we want to go after.
+#
+
+Import-Module -DisableNameChecking -Force ./ROOTRepositoryAccess
+$vinfo = Parse-ROOT-Version $rootVersion
+
+$goodRs = Get-All-ROOT-Downloads | ? {$_.VersionMajor -eq $vinfo.VersionMajor} `
+	| ? {$_.VersionMinor -eq $vinfo.VersionMinor} `
+	| ? {$_.VersionSubMinor -eq $vinfo.VersionSubMinor} `
+	| ? {$_.VersionExtraInfo -eq $vinfo.VersionExtraInfo} `
+	| ? {$_.DownloadType -eq "win32.vc10"} `
+	| ? {$_.FileType -eq "tar.gz"}
+
+if ($goodRs.length > 1)
+{
+	throw New-Object System.InvalidOperationException "There are more than one root's availible for version $rootVersion."
+}
+$goodR = $goodRs[0]
+
