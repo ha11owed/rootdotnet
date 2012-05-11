b27d30ee1de7094fcdb5a5ec9ee1f6fedb5bcad0
gwatts <gwatts@localhost>
2012-05-11 14:12:00 -0700

    First import of basic stuff and test

---
+++ Wrappers/t_Dynamic/t_ROOTCreator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace t_Dynamic
+{
+    /// <summary>
+    /// Make sure various things with ctor and dtor are working. For a real test
+    /// the monilithic wrapper guy should be pretty minimal.
+    /// </summary>
+    [TestClass]
+    public class t_ROOTCreator
+    {
+        [TestMethod]
+        public void TestCreatorCTor()
+        {
+            var c = new ROOTNET.Utility.ROOTCreator();
+        }
+    }
+}
