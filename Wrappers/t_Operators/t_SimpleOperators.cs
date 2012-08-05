33a07e21a8508814e3ca26fa2b1c5ab791db81dc
Gordon <Gordon@EightBall>
2012-08-04 23:18:43 -0400

    Further tests to make sure that we are looking at all possible ways this simple operators can work.

--- Wrappers/t_Operators/t_SimpleOperators.cs
+++ Wrappers/t_Operators/t_SimpleOperators.cs
@@ -84,6 +84,25 @@ namespace t_Operators
             Assert.AreEqual(-4.0, a3.T(), "T");
         }
 
+        [TestMethod]
+        public void TestDoubleTimes()
+        {
+            var a1 = new ROOTNET.NTLorentzVector(1.0, 2.0, 3.0, 4.0);
+            var a3 = a1*a1;
+            Assert.AreEqual(16.0 - 1.0 - 4.0 - 9.0, a3, "magnitude");
+        }
+
+        [TestMethod]
+        public void TestScaling()
+        {
+            var a1 = new ROOTNET.NTLorentzVector(1.0, 2.0, 3.0, 4.0);
+            var a3 = 2.0 * a1;
+            Assert.AreEqual(2.0, a3.X(), "X");
+            Assert.AreEqual(4.0, a3.Y(), "Y");
+            Assert.AreEqual(6.0, a3.Z(), "Z");
+            Assert.AreEqual(8.0, a3.T(), "T");
+        }
+
 #if false
         [TestMethod]
         public void TestAdditionWithObjectInterfaces2()
