8611c481ef57c9ba5c767f01d0ba3feb7fff37bf
Gordon <Gordon@EightBall>
2012-08-08 17:38:08 -0400

    Add a test that tries to create a non-TObject object. And also create a test or two to try and drive the work.

---
+++ Wrappers/t_Dynamic/t_STLObjects.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace t_Dynamic
+{
+    [TestClass]
+    public class t_STLObjects
+    {
+        // Force a reference to the monolithic wrapper guys - so that we can
+        // make sure that the wrappers get loaded into memory.
+#pragma warning disable 0414
+        static ROOTNET.NTObject _obj = null;
+#pragma warning restore 0414
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            ROOTNET.Utility.ROOTCreator.ResetROOTCreator();
+        }
+
+        [TestMethod]
+        public void TestVectorCTor()
+        {
+            dynamic vec = ROOTNET.Utility.ROOTCreator.CreateByName("vector<int>");
+        }
+
+        [TestMethod]
+        public void TestVectorCTorFullSpec()
+        {
+            dynamic vec = ROOTNET.Utility.ROOTCreator.CreateByName("vector<int,allocator<int> >");            
+        }
+    }
+}
