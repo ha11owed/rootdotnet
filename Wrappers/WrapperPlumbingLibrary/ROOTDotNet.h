32d1f3b3cf8ceb059553c6baf3a4e3ce95d2d50d
Gordon <Gordon@EightBall.cern.ch>
2012-08-30 14:53:45 +0200

    Update the version number to 2.6

---
+++ Wrappers/WrapperPlumbingLibrary/ROOTDotNet.h
@@ -0,0 +1,33 @@
+// ROOTDotNet.h
+// Some services for use by everyone else. Very low level.
+//
+#pragma once
+
+#include <string>
+
+class TObject;
+#pragma make_public(TObject)
+
+class TClass;
+#pragma make_public(TClass)
+
+namespace ROOTNET
+{
+	namespace Utility
+	{
+		ref class ROOTDOTNETBaseTObject;
+
+		public ref class ROOTObjectServices
+		{
+		public:
+			static void DoIt () {}
+			// Return the best wrapper we can find for an object that has TObject in its inherritance list.
+			generic<class T> where T: ref class
+				static T GetBestObject (const ::TObject *obj);
+
+			// Return the best wrapper we can find for an object that doesn't have TObject in its inherritance list.
+			static ROOTDOTNETBaseTObject ^ GetBestNonTObjectObject (const void *obj, ::TClass *class_info);
+
+		};
+	}
+}
