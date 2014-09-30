e24898df15ba2b6caa0331141b934696d63b9e3c
gwatts <gwatts@localhost>
2014-09-30 06:24:30 +0200

    Setup for further testing.

--- Wrapper Generators/ROOT.NET Monolithic Converter/Monolithic.cpp
+++ Wrapper Generators/ROOT.NET Monolithic Converter/Monolithic.cpp
@@ -71,7 +71,7 @@ int main()
 
 	/// The below lines are used during debugging in order to build a single (problem) class.
 
-#define UNIT_TEST
+//#define UNIT_TEST
 #ifdef UNIT_TEST
 	// These are the classes needed for all the Wrapper unit tests to succeed.
 	asked_for_class_list.push_back ("TLorentzVector");
@@ -80,15 +80,9 @@ int main()
 	asked_for_class_list.push_back ("TTree");
 	asked_for_class_list.push_back ("TFile");
 #else
-	asked_for_class_list.push_back ("TLorentzVector");
-	asked_for_class_list.push_back ("TAttFill");
-
-	asked_for_class_list.push_back ("TTree");
-	asked_for_class_list.push_back ("TFile");
-
-	asked_for_class_list.push_back ("TClass");
-	asked_for_class_list.push_back ("TSystem");
-	asked_for_class_list.push_back ("TInterpreter");
+	asked_for_class_list.push_back("TList");
+	asked_for_class_list.push_back("TRootSnifferScanRec");
+	libraries_to_load.push_back("libRHTTP");
 #endif
 
 	/// Make sure the libraries that are going to be needed are loaded!
